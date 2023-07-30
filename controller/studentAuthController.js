b5220c4d77b12bc3e5f75891690d6a774e4bd19c
mustardcat999 <129050326+mustardcat999@users.noreply.github.com>
2023-07-31 02:00:50 +0530

    Update studentAuthController.js

--- controller/studentAuthController.js
+++ controller/studentAuthController.js
@@ -5,7 +5,7 @@ import {
   NotFoundError,
   UnAuthenticatedError,
 } from "../errors/index.js";
-import Company from "../models/Company.js";
+import Company from "../models/company.js";
 import Admin from "../models/admin.js";
 
 /**
